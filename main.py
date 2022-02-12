06f9cb7f56f421385db95f210fdacca9ea18be8d
vasyalee <flightsho@gmail.com>
2022-02-12 14:10:20 +0300

    fix precision bug, add new config options

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ client = Client(API_KEY, API_SECRET)
 
 #CONFIG:
 coins = ['BTC', 'ETH', 'BNB'] # Coins for the next trade are going to be randomly chosen from this list
-dca_usdt_value = 12 # Here you can set the amount of USDT that is going to be regularly used for trades
+dca_usdt_value = 10000 # Here you can set the amount of USDT that is going to be regularly used for trades
 dca_periods = {'day': 86400, '3 days': 259200, 'week': 604800, 'month': 2_419_200}
+dca_period = 'week' #Choose the DCA period. "Week" by default
 
 
 def main():
     while True:
         try:
             coin = random.choice(coins)
-            avg_price = client.get_avg_price(symbol=coin + 'USDT')['price']
-            quantity = dca_usdt_value / avg_price
-            print(f'BUYING {coin}')
+            avg_price = round(float(client.get_avg_price(symbol=coin + 'USDT')['price']))
+            quantity = round(dca_usdt_value / avg_price, 5)
+            print(f'BUYING {quantity} {coin} for {dca_usdt_value} USDT with a price of {avg_price} {coin}/USDT. Next trade in {dca_period}')
             buy_coin(coin, quantity)
         except Exception as e:
             print(e)
             print('Some error occured. This is probably because your balance is insufficient.')
-        time.sleep(dca_periods['week'])
+        time.sleep(dca_periods[dca_period])
 
 def buy_coin(currency, currency_quantity):
     order = client.order_market_buy(
@@ -34,6 +35,7 @@ def buy_coin(currency, currency_quantity):
 
 if __name__ == '__main__':
     main()
+
 
 
 
